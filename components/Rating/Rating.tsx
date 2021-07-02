e0730accf5a0e0dafc4c24be82a18c371713c9a3
Sviut <jeleznov88@gmail.com>
2021-07-02 20:22:45 +0300

    add edit rating logic

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import { DetailedHTMLProps, HTMLAttributes, useEffect, useState } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, useEffect, useState, KeyboardEvent } from 'react'
 import cn from 'classnames'
 import styles from './Rating.module.css'
 import StarIcon from './star.svg'
@@ -19,15 +19,42 @@ export const Rating = ({ isEditable = false, rating, setRating, className, ...pr
 
 
 	const constructRating = (currentRating: number) => {
-		const updatedArray = ratingArray.map((rating, i) => {
+		const updatedArray = ratingArray.map((r, i) => {
 			return <StarIcon
 				className={cn(styles.star, {
 					[styles.filled]: i < currentRating,
+					[styles.editable]: isEditable,
 				})}
+				onMouseEnter={() => changeDisplay(i + 1)}
+				onMouseLeave={() => changeDisplay(rating)}
+				onclick={() => onClick(i + 1)}
+				tabIndex={isEditable ? 0 : -1}
+				onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
 			/>
 		})
 		setRatingArray(updatedArray)
 	}
+
+	const changeDisplay = (i: number) => {
+		if (!isEditable) {
+			return
+		}
+		constructRating(i)
+	}
+	const onClick = (i: number) => {
+		if (!isEditable || !setRating) {
+			return
+		}
+		setRating(i)
+	}
+
+	const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+		if (e.code !== 'Space' || !setRating) {
+			return
+		}
+		setRating(i)
+	}
+
 
 	return (
 		<div {...props}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import { Button } from '../components'
+import { Button, P, Rating, Tag } from '../components'
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Button arrow={'down'} appearance={'primary'}>Кнопка</Button>
 			<Button appearance={'ghost'}>Кнопка</Button>
+			<P size="l">dsfsdsf</P>
+			<P size="m">dsfsdsf</P>
+			<P size="s">dsfsdsf</P>
+			<Tag color="grey">тег</Tag>
+			<Rating rating={4}/>
+			<Rating rating={0} isEditable/>
 		</>
 	)
 }

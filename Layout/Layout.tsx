fae0706debf66e648ee4af5dd896b3dfb544a033
Sviut <jeleznov88@gmail.com>
2021-07-05 19:54:14 +0300

    add app context

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,32 +1,35 @@
-import { DetailedHTMLProps, FunctionComponent, HTMLAttributes, ReactNode } from 'react'
-import { Header } from './Header/Header'
-import { Sidebar } from './Sidebar/Sidebar'
-import { Footer } from './Footer/Footer'
+import {DetailedHTMLProps, FunctionComponent, HTMLAttributes, ReactNode} from 'react'
+import {Header} from './Header/Header'
+import {Sidebar} from './Sidebar/Sidebar'
+import {Footer} from './Footer/Footer'
 import styles from './Layout.module.css'
+import {IAppContext, AppContextProvider} from "../context/app.context";
 
 interface LayoutProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-	children: ReactNode
+    children: ReactNode
 }
 
-const Layout = ({ children }: LayoutProps): JSX.Element => {
-	return (
-		<div className={styles.wrapper}>
-			<Header className={styles.header}/>
-			<Sidebar className={styles.sidebar}/>
-			<div className={styles.body}>
-				{children}
-			</div>
-			<Footer className={styles.footer}/>
-		</div>
-	)
+const Layout = ({children}: LayoutProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
+                {children}
+            </div>
+            <Footer className={styles.footer}/>
+        </div>
+    )
 }
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-	return function withLayoutComponent(props: T): JSX.Element {
-		return (
-			<Layout>
-				<Component {...props}/>
-			</Layout>
-		)
-	}
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                <Layout>
+                    <Component {...props}/>
+                </Layout>
+            </AppContextProvider>
+        )
+    }
 }

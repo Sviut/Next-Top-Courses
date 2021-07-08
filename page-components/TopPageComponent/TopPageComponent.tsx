a86d377b8c061c5fb28f8e3010618666fe456749
Sviut <jeleznov88@gmail.com>
2021-07-08 08:51:00 +0300

    add HhData components

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,7 @@
-import {TopPageModel} from "../../interfaces/topPage.interface";
+import {TopLevelCategory, TopPageModel} from "../../interfaces/topPage.interface";
 import {ProductModel} from "../../interfaces/product.interface";
+import {Card, HhDataCards, HTag, Tag} from "../../components";
+import styles from './TopPageComponent.module.css'
 
 export interface TopPageComponentProps {
     firstCategory: number
@@ -9,8 +11,21 @@ export interface TopPageComponentProps {
 
 export const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
     return (
-        <>
-            {products && products.length}
-        </>
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <HTag tag={'h1'}>{page.title}</HTag>
+                {products && <Tag color={'grey'}>{products.length}</Tag>}
+                <span>Сортировка</span>
+            </div>
+            <div>
+                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+            </div>
+            <div className={styles.hhTitle}>
+                <HTag tag='h2'>Вакансии - {page.category}</HTag>
+                <Tag color='red' size='m'>hh.ru</Tag>
+            </div>
+
+            {firstCategory === TopLevelCategory.Courses && <HhDataCards {...page.hh}/>}
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,50 @@
-import { Button, HTag, P, Rating, Tag } from '../components'
-import { withLayout } from '../Layout/Layout'
-import { GetStaticProps } from 'next'
+import {Button, HTag, P, Rating, Tag} from '../components'
+import {withLayout} from '../Layout/Layout'
+import {GetStaticProps} from 'next'
 import axios from 'axios'
-import { MenuItem } from '../interfaces/menu.interface'
+import {MenuItem} from '../interfaces/menu.interface'
 import styles from '../components/Button/Button.module.css'
 import ArrowIcon from '../components/Button/arrow.svg'
 
 interface HomeProps extends Record<string, unknown> {
-	menu: MenuItem[]
-	firstCategory: number
+    menu: MenuItem[]
+    firstCategory: number
 }
 
-function Home({ menu }: HomeProps): JSX.Element {
-	return (
-		<>
-			{/*<Button arrow={'down'} appearance={'primary'}>Кнопка</Button>*/}
-			{/*<Button appearance={'primary'}>Кнопка</Button>*/}
-			{/*<Button appearance={'ghost'}>Кнопка</Button>*/}
-			{/*<Button appearance={'primary'} arrow={'right'}>Кнопка</Button>*/}
-			{/*<Button appearance={'primary'} arrow={'down'}>Кнопка</Button>*/}
-			{/*<P size="l">dsfsdsf</P>*/}
-			{/*<P size="m">dsfsdsf</P>*/}
-			{/*<P size="s">dsfsdsf</P>*/}
-			{/*<Tag color="grey">тег</Tag>*/}
-			{/*<Rating rating={1}/>*/}
-			{/*<Rating rating={2}/>*/}
-			{/*<Rating rating={3}/>*/}
-			{/*<Rating rating={0} isEditable/>*/}
-			{/*<HTag tag={'h1'}>Head</HTag>*/}
-			{/*<HTag tag={'h2'}>Head</HTag>*/}
-			{/*<HTag tag={'h3'}>Head</HTag>*/}
-		</>
-	)
+function Home({menu}: HomeProps): JSX.Element {
+    return (
+        <>
+            {/*<Button arrow={'down'} appearance={'primary'}>Кнопка</Button>*/}
+            {/*<Button appearance={'primary'}>Кнопка</Button>*/}
+            {/*<Button appearance={'ghost'}>Кнопка</Button>*/}
+            {/*<Button appearance={'primary'} arrow={'right'}>Кнопка</Button>*/}
+            {/*<Button appearance={'primary'} arrow={'down'}>Кнопка</Button>*/}
+            {/*<HhDataCards size="l">dsfsdsf</HhDataCards>*/}
+            {/*<HhDataCards size="m">dsfsdsf</HhDataCards>*/}
+            {/*<HhDataCards size="s">dsfsdsf</HhDataCards>*/}
+            {/*<Tag color="grey">тег</Tag>*/}
+            {/*<Rating rating={1}/>*/}
+            {/*<Rating rating={2}/>*/}
+            {/*<Rating rating={3}/>*/}
+            {/*<Rating rating={0} isEditable/>*/}
+            {/*<HTag tag={'h1'}>Head</HTag>*/}
+            {/*<HTag tag={'h2'}>Head</HTag>*/}
+            {/*<HTag tag={'h3'}>Head</HTag>*/}
+        </>
+    )
 }
 
 export default withLayout(Home)
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-	const firstCategory = 0
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-		firstCategory,
-	})
-	return {
-		props: {
-			menu,
-			firstCategory,
-		},
-	}
+    const firstCategory = 0
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory,
+    })
+    return {
+        props: {
+            menu,
+            firstCategory,
+        },
+    }
 }

255d1e4920d853d174d2f226b93843310f33c4ff
Sviut <jeleznov88@gmail.com>
2021-07-23 20:24:09 +0300

    add divider component & styles for product page

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,14 @@
+import {DetailedHTMLProps, HTMLAttributes, ReactNode} from 'react'
+import cn from 'classnames'
+import styles from './Divider.module.css'
+
+interface DividerProps extends DetailedHTMLProps<HTMLAttributes<HTMLHRElement>, HTMLHRElement> {
+}
+
+export const Divider = ({className, ...props}: DividerProps): JSX.Element => {
+    return (
+        <>
+            <hr className={cn(className, styles.hr)} {...props}/>
+        </>
+    )
+}

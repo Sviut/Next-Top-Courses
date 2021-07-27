517ed84a888dbc886ca91b2e4f247107a7cd91a6
Sviut <jeleznov88@gmail.com>
2021-07-27 20:15:26 +0300

    add styles for product page

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,31 +1,30 @@
-import {DetailedHTMLProps, HTMLAttributes, ReactNode} from 'react'
-import cn from 'classnames'
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
 import styles from './Product.module.css'
-import {ProductModel} from "../../interfaces/product.interface";
-import {Card} from "../Card/Card";
-import {Rating} from "../Rating/Rating";
-import {Tag} from "../Tag/Tag";
-import {Button} from "../Button/Button";
-import {Divider} from "../Divider/Divider";
-import {priceRu} from "../../helpers/helpers";
+import { ProductModel } from "../../interfaces/product.interface";
+import { Card } from "../Card/Card";
+import { Rating } from "../Rating/Rating";
+import { Tag } from "../Tag/Tag";
+import { Button } from "../Button/Button";
+import { Divider } from "../Divider/Divider";
+import { declOfNumber, priceRu } from "../../helpers/helpers";
 
 interface ProductProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     product: ProductModel
 }
 
-export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
+export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
     return (
         <Card className={styles.product}>
             <div className={styles.logo}>
-                <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/>
+                <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
             </div>
             <div className={styles.title}>{product.title}</div>
             <div className={styles.price}>
                 {priceRu(product.price)}
                 {product.oldPrice && <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}
-            </div> 
+            </div>
             <div className={styles.credit}>{priceRu(product.credit)}/<span className={styles.month}>месяц</span></div>
-            <div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
+            <div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
             <div className={styles.tags}>
                 {product.categories.map(category => {
                     return (
@@ -39,10 +38,18 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
             </div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.ratingTitle}>{product.reviewCount} отзывов</div>
-            <Divider className={styles.hr}/>
+            <div className={styles.ratingTitle}>{product.reviewCount} {declOfNumber(product.reviewCount, ['отзыв', 'отзыва', "отзывов"])}</div>
+            <Divider className={styles.hr} />
             <div className={styles.description}>{product.description}</div>
-            <div className={styles.features}>фичи</div>
+            <div className={styles.features}>
+                {product.characteristics.map(c => (
+                    <div className={styles.characteristics} key={c.name}>
+                        <span className={styles.characteristicsName}>{c.name}</span>
+                        <span className={styles.characteristicsDots}/>
+                        <span>{c.value}</span>
+                    </div>
+                ))}
+            </div>
             <div className={styles.advBlock}>
                 {product.advantages && <div className={styles.advantages}>
                     <div className={styles.advTitle}>Преимущества</div>
@@ -53,7 +60,7 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
                     <div>{product.disadvantages}</div>
                 </div>}
             </div>
-            <Divider className={styles.hr}/>
+            <Divider className={styles.hr} />
             <div className={styles.actions}>
                 <Button appearance={'primary'}>Узнать подробнее</Button>
                 <Button appearance={'ghost'} arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
